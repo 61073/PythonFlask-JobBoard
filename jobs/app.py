bcc30c053a27caec4a8a47664c4181e4068614f5
61073 <mike.wood@mastek.com>
2019-02-08 11:57:40 +0000

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -56,7 +56,7 @@ def review(employer_id):
         title = request.form['title']
         status = request.form['status']
         
-        date = datetime.datetime.now().strfttime("%m/%d/%y")
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
                     (review, rating, title, date, status, employer_id), commit=True)
         
